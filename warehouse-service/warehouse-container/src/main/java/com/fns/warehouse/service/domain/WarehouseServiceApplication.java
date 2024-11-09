b8b173c47302b9aa09b60b9ca18f8aba00d0f4d5
fadellh <fadel.lukman.dev@gmail.com>
2024-11-09 19:30:23 +0700

    Add warehouse-service to wh-multi

---
+++ warehouse-service/warehouse-container/src/main/java/com/fns/warehouse/service/domain/WarehouseServiceApplication.java
@@ -0,0 +1,34 @@
+package com.fns.warehouse.service.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = { "com.fns.warehouse.service.dataaccess", "com.fns.dataaccess", "com.fns.warehouse.service.domain.ports.output.repository" })
+@EntityScan(basePackages = { "com.fns.warehouse.service.dataaccess", "com.fns.warehouse.dataaccess", "com.fns.warehouse.service.messaging","com.fns.warehouse.messaging" })
+@SpringBootApplication(scanBasePackages = "com.fns")
+public class WarehouseServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseServiceApplication.class, args);
+    }
+}
+
+
+//@SpringBootApplication(scanBasePackages = "com.fns")
+//@EnableJpaRepositories(basePackages = {
+//        "com.fns.warehouse.service.dataaccess",
+//        "com.fns.dataaccess",
+//        "com.fns.warehouse.service.domain.ports.output.repository"
+//})
+//@EntityScan(basePackages = {
+//        "com.fns.warehouse.service.dataaccess",
+//        "com.fns.warehouse.dataaccess",
+//        "com.fns.warehouse.service.messaging",
+//        "com.fns.warehouse.messaging"
+//})
+//public class WarehouseServiceApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(WarehouseServiceApplication.class, args);
+//    }
+//}
